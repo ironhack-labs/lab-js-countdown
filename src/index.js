e0a32998481d26ef3a15f7d7391b5ddd4b3072fd
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2023-12-05 10:51:38 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const DURATION = 10; // 10 seconds
+let remainingTime = DURATION; // Countdown starting from 10
+let timer = null; // Variable to store the interval
+
+
+
+// ITERATION 1: Add event listener to the start button
+
+// Your code goes here ...
+
+
+
+
+// ITERATION 2: Start Countdown
+function startCountdown() {
+  console.log("startCountdown called!");
+
+
+  // Your code goes here ...
+}
+
+
+
+
+// ITERATION 3: Show Toast
+function showToast(message) {
+  console.log("showToast called!");
+
+  // Your code goes here ...
+
+
+
+
+  // BONUS: ITERATION 4: TOAST CLOSE BUTTON
+
+  // Your code goes here ...
+
+}
