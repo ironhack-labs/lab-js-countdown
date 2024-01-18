1ac7c77726a68af94cb759fbc319cdb7184a7bb2
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-18 16:09:37 +0100

    bonus 4 done

--- src/index.js
+++ src/index.js
@@ -2,45 +2,53 @@ const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
-
-
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
 let startButton = document.getElementById("start-btn");
 startButton.addEventListener("click", () => {
-  startCountdown()
-})
+  startCountdown();
+});
 
-
-let display = document.getElementById('time')
+let display = document.getElementById("time");
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
+  startButton.disabled = true;
   let i = remainingTime;
   const intervalID = setInterval(function () {
-    display.innerHTML = i
+    display.innerHTML = i;
     i--;
     if (i < 0) {
       clearInterval(intervalID);
+      showToast();
+      closeToast();
+      startButton.disabled = false;
     }
   }, 1000);
 }
 
-
-
+const toast = document.getElementById("toast");
 
 // ITERATION 3: Show Toast
 function showToast(message) {
+  toast.classList.add("show");
   console.log("showToast called!");
+  setTimeout(function () {
+    toast.classList.remove("show");
+    console.log("has been closed!");
+  }, 3000);
+}
 
-  // Your code goes here ...
-
-
-
-
-  // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
-  // Your code goes here ...
-
+// BONUS: ITERATION 4: TOAST CLOSE BUTTON
+function closeToast() {
+  let toastCloseButton = document.getElementById("close-toast");
+  toastCloseButton.addEventListener(
+    "click",
+    () => {
+      toast.classList.remove("show");
+      console.log("has been closed!");
+    },
+    100
+  );
 }
