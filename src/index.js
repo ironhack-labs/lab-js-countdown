7566f19291c314e21d571567cdb751f49b3c12cc
Lu Marconi <lumarconi21@gmail.com>
2024-01-25 22:04:56 +0100

    Lab Done

--- src/index.js
+++ src/index.js
@@ -2,37 +2,49 @@ const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
-
-
 // ITERATION 1: Add event listener to the start button
-
 // Your code goes here ...
+const start = document.getElementById("start-btn");
 
-
-
+start.addEventListener("click", function () {
+  startCountdown();
+});
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
 
-
   // Your code goes here ...
+  let divTime = document.getElementById("time");
+  let countDown = setInterval(function () {
+    remainingTime--;
+    divTime.innerText = remainingTime;
+    if (remainingTime === 0) {
+      clearInterval(countDown);
+      showToast();
+    }
+  }, 1000);
 }
 
-
-
-
 // ITERATION 3: Show Toast
+// Your code goes here ...
+
+//let show = document.getElementsByClassName("show");//
+let showCard = document.getElementById("toast");
+let closeBtn = document.getElementById("close-toast");
+
 function showToast(message) {
   console.log("showToast called!");
 
-  // Your code goes here ...
-
-
-
+  showCard.classList.add("show");
+  setTimeout(function () {
+    showCard.classList.remove("show");
+  }, 3000);
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
-
+  closeBtn.addEventListener("click", function () {
+    showCard.classList.remove("show");
+  });
 }
