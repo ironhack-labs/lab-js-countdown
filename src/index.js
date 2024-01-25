7a5def06ec08350377a27346ba36ec185af20356
pedro roca de togores <pedrobgrdt@gmail.com>
2024-01-25 20:48:57 +0100

    popino.loco

--- src/index.js
+++ src/index.js
@@ -6,33 +6,71 @@ let timer = null; // Variable to store the interval
 
 // ITERATION 1: Add event listener to the start button
 
-// Your code goes here ...
+let startButton = document.querySelector("#start-btn")
+
+startButton.onclick = () => {
+
+  const Countdown = setInterval(() => {
+    remainingTime--
+    document.querySelector("#time").innerHTML = remainingTime
+
+    if (remainingTime === 0) {
+      clearInterval(Countdown)
+      showToast()
+    }
+
+  }, 100)
 
 
+}
 
 
 // ITERATION 2: Start Countdown
-function startCountdown() {
-  console.log("startCountdown called!");
-
-
-  // Your code goes here ...
-}
-
-
-
-
+// function startCountdown() {
+//   console.log("startCountdown called!");
+// Your code goes here ...
+// }
 // ITERATION 3: Show Toast
 function showToast(message) {
-  console.log("showToast called!");
-
-  // Your code goes here ...
 
 
+  document.querySelector('#toast').classList.add('show')
+
+  const intervalID = setTimeout(() => {
+    document.querySelector(".show").innerHTML = "popino"
+  }, 3000)
 
 
-  // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
-  // Your code goes here ...
+
+  const closeToast = document.querySelector("#close-toast")
+
+  closeToast.onclick = () => {
+    closeToast("show")
+  }
 
 }
+
+console.log("showToast called!");
+// if (remainingTime === 0) {
+
+//   const intervalID = setTimeout(() => {
+//     document.querySelector(".show").innerHTML = "popino"
+//   }, 3000)
+
+
+// }
+
+// //
+// document.getElementById("myDIV").classList.toggle("myStyle");
+
+
+
+// Your code goes here ...
+
+
+// BONUS: ITERATION 4: TOAST CLOSE BUTTON
+
+
+
+
