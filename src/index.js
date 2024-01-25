f67988686c57cce469802c1ddc52581152c6cd33
Laia Navalon <laianavalon@gmail.com>
2024-01-25 16:13:40 +0100

    Iteration 3-5 done

--- src/index.js
+++ src/index.js
@@ -11,41 +11,63 @@ const startButton = document.querySelector("#start-btn")
 
 startButton.addEventListener('click', () => {
   startCountdown();
+  startButton.disabled = true;
 })
 
-const time = document.querySelector('div#time')
+
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
 
   // Your code goes here ...
-  console.log("startCountdown called!");
+  const time = document.querySelector('div#time')
 
   const intervalId = setInterval(() => {
-    console.log(remainingTime)
-    time.innerText = remainingTime;
+    if (remainingTime == 10) {
+      showToast("⏰ Final countdown! ⏰");
+    }
+    if (remainingTime == 5) {
+      showToast("Start the engines! 💥");
+    }
+
     remainingTime--;
+    time.innerText = remainingTime;
 
     if (remainingTime < 0) {
       clearInterval(intervalId);
+      showToast("Lift off! 🚀");
     }
   }, 1000);
 }
 
 
 
-
 // ITERATION 3: Show Toast
+
 function showToast(message) {
-  console.log("showToast called!");
 
   // Your code goes here ...
+  const toastMessage = document.querySelector("div#toast");
 
+  toastMessage.classList.add('show');
+
+  setTimeout(() => {
+    toastMessage.classList.remove('show');
+  }, 3000)
 
 
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
+  const closeToast = document.querySelector("#close-toast");
+  closeToast.addEventListener('click', () => {
+    toastMessage.classList.remove('show');
+  })
 
+
+  // BONUS: ITERATION 5
+
+  const toastText = document.querySelector("#toast-message")
+  toastText.innerText = message;
 }
