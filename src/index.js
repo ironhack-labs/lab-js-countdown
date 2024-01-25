59c9185e3488375cd50bc2da724a59f25f4f87c6
„Aristidis <ari86@MacBook-Pro.local>
2024-01-25 23:01:00 +0100

    Solved lab

--- src/index.js
+++ src/index.js
@@ -2,25 +2,29 @@ const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
-
-
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
 
-
-
-
+document.getElementById("start-btn").addEventListener("click", startCountdown);
 // ITERATION 2: Start Countdown
+
+// Your code goes here ...
+
 function startCountdown() {
+  let counter = 10;
+  const timeElement = document.getElementById("time");
+  const countDown = setInterval(function () {
+    timeElement.innerText = counter;
+    if (counter === 1) {
+      clearInterval(countDown);
+    }
+    console.log(counter);
+    counter--;
+  }, 1000);
+
   console.log("startCountdown called!");
-
-
-  // Your code goes here ...
 }
-
-
-
 
 // ITERATION 3: Show Toast
 function showToast(message) {
@@ -28,11 +32,7 @@ function showToast(message) {
 
   // Your code goes here ...
 
-
-
-
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
-
 }
