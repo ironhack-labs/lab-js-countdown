f0bd3a82cd50e34c912bbf354adcceb2b48a2d1a
Francisco Estepa Guerra <estepaguerrafrancisco@gmail.com>
2024-02-02 19:27:28 +0100

    solved lab

--- src/index.js
+++ src/index.js
@@ -1,22 +1,54 @@
 const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
+let timeInDom = document.querySelector("#time")
+let toastText = document.querySelector("#toast")
+let showToastMessage = document.querySelector("#toast-message")
 
 
 
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
+let startBotton = document.querySelector("#start-btn");
+startBotton.addEventListener("click", startCountdown);
+startBotton.disabled = false;
 
 
+let setIntervalId;
+let setInterval2Id;
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
-
-
   // Your code goes here ...
+  setIntervalId = window.setInterval(() => {
+    startBotton.disabled = true;
+    remainingTime--
+    timeInDom.innerText = remainingTime
+
+    if (remainingTime === 9) {
+      showToast(showToastMessage.innerText)
+    }
+
+    if (remainingTime === 5) {
+      showToast(showToastMessage.innerText)
+    }  
+
+    if (remainingTime === 0) {
+      showToast(showToastMessage.innerText)
+    }
+
+    if (remainingTime === 0) {
+      clearInterval(setIntervalId)
+    }
+    console.log(remainingTime)
+
+  }, 1000)
+
+  
+  
 }
 
 
@@ -27,12 +59,45 @@ function showToast(message) {
   console.log("showToast called!");
 
   // Your code goes here ...
+  toastText
+  console.log(toastText)
+  toastText.classList.add("show")
 
+  if (remainingTime === 9) {
+    showToastMessage.innerText = "⏰ Final countdown!"
+  }
 
+  if (remainingTime === 5) {
+    showToastMessage.innerText = "Start the engines! 💥"
+  }  
 
+  if (remainingTime === 0) {
+    showToastMessage.innerText = "Lift off! 🚀"
+  }
+  //showToastMessage.innerText = "Se acabó el tiempo!"
 
-  // BONUS: ITERATION 4: TOAST CLOSE BUTTON
+  setInterval2Id = window.setInterval(() => {
+    
 
-  // Your code goes here ...
+    if (remainingTime < 0) {
+      clearInterval(setInterval2Id)
+    }
+  }, 1500)
 
+  setTimeout(() => {
+    toastText.classList.remove("show");
+  }, 3000);
+  
 }
+
+// BONUS: ITERATION 4: TOAST CLOSE BUTTON
+// Your code goes here ...
+
+let finishShowCastButton = document.querySelector("#close-toast")
+finishShowCastButton.addEventListener("click", () => {
+  toastText.classList.remove("show");
+})
+
+
+
+// BONUS: ITERATION 5: MORE TOATS
