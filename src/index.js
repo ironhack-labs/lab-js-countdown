34a2b5771d0bb958daab1adbedb45158ce10d208
Hemyulin <daniel.budiansky@protonmail.com>
2024-02-22 14:27:31 +0100

    init

--- src/index.js
+++ src/index.js
@@ -2,25 +2,20 @@ const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
-
-
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
 
+const timeElement = document.getElementById("time");
+const startButton = document.getElementById("start-btn");
 
-
+startButton.addEventListener("click", startCountdown);
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
-
-
-  // Your code goes here ...
 }
-
-
-
+// Your code goes here ...
 
 // ITERATION 3: Show Toast
 function showToast(message) {
@@ -28,11 +23,7 @@ function showToast(message) {
 
   // Your code goes here ...
 
-
-
-
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
-
 }
