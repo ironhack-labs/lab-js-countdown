61d3cf98b6e29715d25df035bc0aac9cbd6fe4ee
Hemyulin <daniel.budiansky@protonmail.com>
2024-02-22 14:57:14 +0100

    it 4 done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const DURATION = 10; // 10 seconds
+const DURATION = 1; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
@@ -8,8 +8,11 @@ let timer = null; // Variable to store the interval
 
 const timeElement = document.getElementById("time");
 const startButton = document.getElementById("start-btn");
+const toastElement = document.getElementById("toast");
+const closeToastElement = document.getElementById("close-toast");
 
 startButton.addEventListener("click", startCountdown);
+closeToastElement.addEventListener("click", closeToast);
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
@@ -32,10 +35,17 @@ function startCountdown() {
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
+  toastElement.classList.add("show");
+  setTimeout(() => {
+    console.log("showToast GONE!");
+    toastElement.classList.remove("show");
+  }, "3000");
 
   // Your code goes here ...
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
+}
+function closeToast() {
+  toastElement.classList.remove("show");
   // Your code goes here ...
 }
