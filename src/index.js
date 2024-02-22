8c8be08c247a0ada7ebfa97d64f02b97aec458ab
Mariana <marianabatalhadosloios@gmail.com>
2024-02-22 22:44:41 +0000

    lab done

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
 const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
-
-
-
+let timerContainer = document.getElementById("time");
 // ITERATION 1: Add event listener to the start button
+const startButton = document.getElementById("start-btn");
 
-// Your code goes here ...
-
-
-
+startButton.addEventListener("click", startCountdown);
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
+  timer = setInterval(function () {
+    startButton.disabled = true;
+    remainingTime--;
+    timerContainer.innerHTML = remainingTime;
 
-
-  // Your code goes here ...
+    if (remainingTime === 0) {
+      clearInterval(timer);
+      showToast();
+    }
+  }, 1000);
 }
-
-
-
 
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
-
-  // Your code goes here ...
-
-
-
+  let toaster = document.getElementById("toast");
+  toaster.classList.add("show");
+  setTimeout(function () {
+    toaster.classList.remove("show");
+  }, 3000);
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
-  // Your code goes here ...
-
+  document.getElementById("close-toast").addEventListener("click", function () {
+    toaster.classList.remove("show");
+  });
 }
