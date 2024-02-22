5a09e45241e5a6d5ef6f910ef7f784ed9835aae8
Piero Schenato <piero.schenato@gmail.com>
2024-02-22 15:30:14 +0100

    Your commit message here

--- src/index.js
+++ src/index.js
@@ -11,19 +11,47 @@ let timer = null; // Variable to store the interval
 
 
 
-// ITERATION 2: Start Countdown
-function startCountdown() {
-  console.log("startCountdown called!");
 
+// ITERATION 2: Start Countdown
+const startBtn = document.getElementById('start-btn');
+const countDownDisplay = document.getElementById('Countdown')
+const timeElement = document.getElementById('time')
+let countDownValue = 10;
+let intervalId;
+
+function startCountdown() {
+ intervalId = setInterval(() => {
+    countDownValue --;
+    countDownDisplay.textContent = countDownValue;
+    timeElement.innerText = countDownValue;
+  
+  if (countDownValue === 0) {
+    clearInterval(intervalId);
+    console.log ('stop the clock')
+  }
+},1000);
+}
+startBtn.addEventListener('click', () => {
+  console.log('start button clicked');
+  startCountdown()
+})
+ 
 
   // Your code goes here ...
-}
+
 
 
 
 
 // ITERATION 3: Show Toast
+const list = toast.classList
+list.add('show');
+
 function showToast(message) {
+  setTimeout(showToast,3000)
+  clearTimeout(showToast,0)
+  }
+
   console.log("showToast called!");
 
   // Your code goes here ...
@@ -35,4 +63,4 @@ function showToast(message) {
 
   // Your code goes here ...
 
-}
+
