602adee33a087f45a64e113883e54be523088b80
Michael Nguyen <michael.nguyen@live.nl>
2024-03-26 18:59:12 +0100

    interation 1-3 done

--- src/index.js
+++ src/index.js
@@ -1,38 +1,42 @@
 const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
-
+const timeElement = document.querySelector("#time")
 
 
 // ITERATION 1: Add event listener to the start button
 
-// Your code goes here ...
-
+const startBtnElement = document.querySelector("#start-btn")
+startBtnElement.addEventListener("click",()=>{
+  startCountdown();
+})
 
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
-
-
-  // Your code goes here ...
+  timer = setInterval(() => {
+    remainingTime--;
+    timeElement.innerText = remainingTime;
+    if (remainingTime === 0) {
+      clearInterval(timer);
+      showToast(); // Call showToast() when remainingTime reaches 0
+    }
+  }, 1000);
 }
 
 
 
 
 // ITERATION 3: Show Toast
-function showToast(message) {
-  console.log("showToast called!");
-
-  // Your code goes here ...
-
-
-
-
-  // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
-  // Your code goes here ...
-
+function showToast() {
+ 
+  const toast = document.querySelector("#toast");
+  toast.classList.add("show");
+ 
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 3000);
 }
+
